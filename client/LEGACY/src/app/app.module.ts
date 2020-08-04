912ecbd39674141469ea5dbe7ab8127f21498bd2
omaryakoubi <your_email_address@example.com>
2020-08-04 14:43:13 +0100

    SIGNIN ONLY TO GET VALUE FROM INPUT AND DONE

--- client/LEGACY/src/app/app.module.ts
+++ client/LEGACY/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, SignUpComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- client/LEGACY/src/app/sign-up/sign-up.component.ts
+++ client/LEGACY/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  userName: string;
+  addressMail: string;
+  firstName: string;
+  lastName: string;
+  password: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  postData() {
+    const url: string = 'http://localhost:8000/signUp';
+    this.http
+      .post(url, {
+        userName: this.userName,
+        addressMail: this.userName,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        password: this.password,
+      })
+      .toPromise()
+      .then((data: string) => {
+        console.log(data);
+      });
   }
 
+  ngOnInit(): void {}
 }
